12d987d3b4a2fd3eca20d3b1eb6a471ceba0ff9a
Yu Mu <muyu0117@live.cn>
2020-10-26 16:42:31 -0400

    add more tests

--- cipher/test/crypt.py
+++ cipher/test/crypt.py
@@ -1,5 +1,6 @@
 import pytest
 from sans_program_project.cipher import cipher_engine
+from sans_program_project.cipher.cipher import Cipher 
 
 
 def test_ReverseCipher():
@@ -18,7 +19,21 @@ def test_CaesarCipher():
     inst1 = cipher_engine.CaesarCipher(key=3)
     assert inst1.encrypt('abcd') == 'defg'
     assert inst1.decrypt('defg') == 'abcd'
-
+    
     inst2 = cipher_engine.CaesarCipher(key=3)
     assert inst2.encrypt('Hello, World!') == 'Khoor/#Zruog$'
     assert inst2.decrypt('Khoor/#Zruog$') == 'Hello, World!'
+    
+    inst3 = cipher_engine.CaesarCipher(key=0)
+    assert inst3.encrypt('Hello, World!') == 'Hello, World!'
+    assert inst3.decrypt('Hello, World!') == 'Hello, World!'
+ 
+    inst4 = cipher_engine.CaesarCipher(key=100)
+    assert inst4.encrypt('Hello, World!') == ',IPPS\x10\x04;SVPH\x05'
+    assert inst4.decrypt(',IPPS\x10\x04;SVPH\x05') == 'Hello, World!'
+
+    inst5 = cipher_engine.CaesarCipher(key=3000)
+    test = inst5.encrypt('Hello, World!')
+    inst5.decrypt(test) == 'Hello, World!'
+
+    
