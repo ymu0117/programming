19a7da2f23fda6cf109d31e40ff2c7339c32bc39
Yu Mu <muyu0117@live.cn>
2020-10-27 19:10:48 -0400

    add dcp

---
+++ dcp/pouring.py
@@ -0,0 +1,8 @@
+
+
+def pour_problem(X, Y, goal, start=(0, 0)):
+    """X and Y are the capacity of glasses; (x, y) is current fill levels
+    and represents a state. The goal is a level that can be in either glass. 
+    Start at start state and follow successors until we reach the goal. 
+    Keep track of frontier and previously explored; fail when no frontier."""
+    pass 
