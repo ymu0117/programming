3807f5704fd6f2a667ba36df882101e9028c10aa
Yu Mu <muyu0117@live.cn>
2020-11-01 20:22:06 -0500

    commit before checkout

--- cipher/test/crypt.py
+++ cipher/test/crypt.py
@@ -1,7 +1,7 @@
 import pytest
 from programming.cipher import cipher_engine
 from programming.cipher.cipher import Cipher
-from programming.cipher.cipher_hacker import cipher_hacker 
+from programming.cipher.cipher_hacker import cipher_hacker
 
 
 def test_ReverseCipher():
@@ -22,44 +22,44 @@ def test_CaesarCipher():
     assert inst1.decrypt('defg') == 'abcd'
 
 
-def test_CaesarCipher_key3(): 
+def test_CaesarCipher_key3():
     inst2 = cipher_engine.CaesarCipher(key=3)
     ciphertext = inst2.encrypt('Hello, World!')
     assert inst2.decrypt(ciphertext) == 'Hello, World!'
 
 
-def test_CaesarCipher_key0(): 
+def test_CaesarCipher_key0():
     inst3 = cipher_engine.CaesarCipher(key=0)
     assert inst3.encrypt('Hello, World!') == 'Hello, World!'
     assert inst3.decrypt('Hello, World!') == 'Hello, World!'
 
 
-def test_CaesarCipher_key100(): 
+def test_CaesarCipher_key100():
     inst4 = cipher_engine.CaesarCipher(key=100)
     ciphertext = inst4.encrypt('Hello, World!')
     assert inst4.decrypt(ciphertext) == 'Hello, World!'
 
 
-def test_CaesarCipher_key3000(): 
+def test_CaesarCipher_key3000():
     inst5 = cipher_engine.CaesarCipher(key=3000)
     test = inst5.encrypt('Hello, World!')
     assert inst5.decrypt(test) == 'Hello, World!'
 
 
-def test_CaesarCipher_alphabeta(): 
+def test_CaesarCipher_alphabeta():
     plaintext = 'αβγδεζηθικλμνξοπρςστυφχψ'
     inst6 = cipher_engine.CaesarCipher(key=3)
     test = inst6.encrypt(plaintext)
     assert inst6.decrypt(test) == 'αβγδεζηθικλμνξοπρςστυφχψ'
 
 
-def test_CaesarCipher_Emojis(): 
-    plaintext = '\U0001f600\U0001F606\U0001F923'    # Emojis 
+def test_CaesarCipher_Emojis():
+    plaintext = '\U0001f600\U0001F606\U0001F923'    # Emojis
     inst7 = cipher_engine.CaesarCipher(key=30)
     test = inst7.encrypt(plaintext)
-    assert inst7.decrypt(test) == plaintext 
+    assert inst7.decrypt(test) == plaintext
 
 
 def test_cipher_hacker():
-    pass 
-    
+    pass
+
